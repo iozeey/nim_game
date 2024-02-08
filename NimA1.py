dcb8d3bca068c6ef52df0438287ad058a8a81e6a
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2024-02-08 21:56:23 +0000

    Nim human vs human

--- NimA1.py
+++ NimA1.py
@@ -1,6 +1,6 @@
 import random
-heap = [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
+# valid random number
 def get_random_number():
     return random.randint(1,3)
 
@@ -19,15 +19,51 @@ def nim_best(left_sticks):
         nim_best(left_sticks)
     return legal_move
 
-def nim_human(left_sticks):
-    
-    while True:
-        move = int(input("Enter your move"))
+def nim_human():    
+    move = int(input("Enter your move: "))
 
-        if  move < len(left_sticks):
-            return move
+    if  move > 0 and move <= 3:
+        return move
+    else:
+        nim_human()
         
 def get_player_name_human():
-  return [(input("Player 1 Name")), (input("Player 2 Name"))]
+  return [input("Player 1 Name: "), input("Player 2 Name: ")]
 
-print(get_player_name_human())
+def is_over(heap):
+    return len(heap) == 0
+
+def execute_on_heap(heap, size):
+    return heap[size:]
+
+def game_controller():
+    # via list comprehension
+    heap = [1 for x in range(int(input("Enter Heap Size: ")))]
+    # prompt
+    print("Select corresponding digit for\n")
+    print("1 for Human vs Human\n")
+    print("2 for Computer vs Human\n")
+    print("3 for Computer vs Computer\n")
+
+    choice = int(input("Enter Your choice: "))
+    players = get_player_name_human()
+    
+    # who is playing currently
+    current_turn = 0
+    
+    # run game
+    while True:
+        if choice == 1 and len(heap) > 0:
+            print(players[current_turn % 2]+"'s turn ")
+            move = nim_human()
+            heap = execute_on_heap(heap, move)
+            
+            if is_over(heap):  
+                print(players[current_turn % 2]+" won!!!")     
+                return  
+            # for next player
+            current_turn += 1
+
+        print(heap)
+
+print(game_controller())
