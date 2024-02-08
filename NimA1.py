7f5bb3ab9bbdea5fb21368841dbf2891e5874b28
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2024-02-08 17:37:08 +0000

    Add best nim move function

--- NimA1.py
+++ NimA1.py
@@ -1,9 +1,23 @@
 import random
-heap = [1,1,1,1,1,1,1,1,1,1,1]
+heap = [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+def get_random_number():
+    return random.randint(1,3)
 
 # produce legal move
 def nim(left_sticks):    
     if len(left_sticks) > 0:
-        return random.randint(1,3)
+        return get_random_number()
 
-print(nim(heap))
+# produce legal optimal move
+    # find out the best move if user take they will wins
+    # After doing some exercise we found if a player(1 or 2) see 4 bars left on the second last step(before end of game) then the last player will win as he will have sticks(which is winning rule)
+def nim_best(left_sticks):
+    legal_move = get_random_number()
+
+    if legal_move % 4 == 0:
+        nim_best(left_sticks)
+    return legal_move
+         
+
+print(nim_best(heap))
