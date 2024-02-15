32c12c2829f327e4f6ffca6bb8e03127cd340bac
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2024-02-15 18:06:54 +0000

    Divide the array

--- Sort.py
+++ Sort.py
@@ -4,25 +4,30 @@ import random
 import pdb;
 
 def show_Print(list):
-    print(list)
-
-def recursive_sort(list):
-    print(list)
     if int(len(list)) == 2:
         if(list[0] > list[1]):
             return [list[1], list[0]]
         return list
-    
-    shrink_list =  list[0 : len(list) - 2]
-    sorted = recursive_sort(shrink_list)
+
+def recursively_divide(list):
     print(list)
     
+    if(len(list)) == 1:
+        return list
+    
+    mid =  int(len(list) / 2)
+    left =  list[0 : mid]
+    right =  list[mid : ]
+
+    left_list = recursively_divide(left)
+
+    print(left_list)
 
 def sort4(a1,a2,a3,a4):    
     list  = [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18, 2, 10]
     # list = [random.randint(1,20) for i in range(20)]
     # [a1,a2,a3,a4]
-    temp_list = recursive_sort(list)
+    temp_list = recursively_divide(list)
     # do using recursion
     # for p_index, parent_item in enumerate(list):
     #     for c_index, current in enumerate(list):
@@ -36,6 +41,19 @@ def sort4(a1,a2,a3,a4):
     # min_1_2 = min(min_1_2,a3)
 
 print(sort4(11,2,13,4));
+
+# output for divide strategy of left part of list via recursion
+# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18, 2, 10]
+# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20]
+# [12, 2, 1, 13, 5]
+# [12, 2]
+# [12]
+# [12]
+# None
+# None
+# None
+# None
+
 # out put of recursive calls for stack variable
 # [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18, 2, 10]
 # [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18]
