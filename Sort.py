e63679802f6d887bd0d101a6492ad6825749d7aa
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2024-02-15 20:29:35 +0000

    Complete the algorithm

--- Sort.py
+++ Sort.py
@@ -1,18 +1,24 @@
 #! /usr/local/bin/python3
 
-import random
-import pdb;
+def show_Print(left,right):    
+    result = []
+    left_index, right_index = 0, 0
 
-def show_Print(list):
-    if int(len(list)) == 2:
-        if(list[0] > list[1]):
-            return [list[1], list[0]]
-        return list
-
-def recursively_divide(list):
-    print(list)
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
     
-    if(len(list)) == 1:
+    # copy the rest
+    result.extend(left[left_index: ])
+    result.extend(right[right_index: ])
+
+    return result
+def recursively_divide(list):
+    if(len(list)) <= 1:
         return list
     
     mid =  int(len(list) / 2)
@@ -20,57 +26,16 @@ def recursively_divide(list):
     right =  list[mid : ]
 
     left_list = recursively_divide(left)
+    right_list = recursively_divide(right)
 
-    print(left_list)
+    return show_Print(left_list, right_list)
 
 def sort4(a1,a2,a3,a4):    
-    list  = [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18, 2, 10]
-    # list = [random.randint(1,20) for i in range(20)]
+    # list = [random.randint(1,20) for i in range(4)]
     # [a1,a2,a3,a4]
+    list  = [12, 2, 1, 13, 5, 18, 13, 20]
     temp_list = recursively_divide(list)
-    # do using recursion
-    # for p_index, parent_item in enumerate(list):
-    #     for c_index, current in enumerate(list):
-    #         if(p_index != c_index):
-    #             minimum = min(parent_item,current)
-    #             print("parent, child and min %s %s is %s" %(parent_item, current, minimum))
-    #     # print(minimum)
-    #     temp_list.append(minimum)
+ 
     return temp_list
-    # min_1_2 = min(a1,a2)
-    # min_1_2 = min(min_1_2,a3)
 
 print(sort4(11,2,13,4));
-
-# output for divide strategy of left part of list via recursion
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18, 2, 10]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20]
-# [12, 2, 1, 13, 5]
-# [12, 2]
-# [12]
-# [12]
-# None
-# None
-# None
-# None
-
-# out put of recursive calls for stack variable
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18, 2, 10]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20]
-# [12, 2, 1, 13, 5, 18, 13, 20]
-# [12, 2, 1, 13, 5, 18]
-# [12, 2, 1, 13]
-# [12, 2]
-# [12, 2, 1, 13]
-# [12, 2, 1, 13, 5, 18]
-# [12, 2, 1, 13, 5, 18, 13, 20]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18]
-# [12, 2, 1, 13, 5, 18, 13, 20, 18, 20, 6, 14, 17, 11, 16, 11, 5, 18, 2, 10]
