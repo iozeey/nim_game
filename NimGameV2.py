41e0f6e19ca81ebf2aef2393ec98019b48272802
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2024-02-16 22:33:20 +0000

    Add Computer Vs Human and Computer vs Computer Fix Recursive bug by replacing move generation via while loop

--- NimGameV2.py
+++ NimGameV2.py
@@ -18,26 +18,43 @@ def nim(left_sticks):
 def nim_best(left_sticks):
     legal_move = get_random_number()
 
-    if legal_move % 4 == 0:
+    if legal_move % 4 == 0 or legal_move > left_sticks:
         nim_best(left_sticks)
+    
     return legal_move
 
-def nim_human():    
-    move = int(input("Enter your move: "))
-
-    if  move > 0 and move <= 3:
-        return move
-    else:
-        nim_human()
+def nim_human(left_sticks):    
+    while True:
+        move = int(input("Enter your move: "))
+    
+        if  move > 0 and move <= 3 and move <= left_sticks:
+            return move
+        else:
+            continue
+            # this recursion is faulty when it returns then the caller points receives the old results i think due to stack clearing strategy
+            # nim_human(left_sticks)
         
-def get_player_name_human():
-  return [input("Player 1 Name: "), input("Player 2 Name: ")]
-
+def get_player_names(choice):
+    if choice == 1:
+        return [input("Player 1 Name: "), input("Player 2 Name: ")]
+    elif choice == 2:
+        return ["Computer", input("Player 2 Name: ")]
+    else:
+        return ["Computer 1", "Computer 2"]
+    
 def is_over(heap):
     return len(heap) == 0
 
 def execute_on_heap(heap, size):
     return heap[size:]
+
+def get_moves(left_sticks, choice):
+    if choice == 1:
+        return nim_human(left_sticks)
+    elif choice == 2:
+        return nim_human(left_sticks) 
+    elif choice == 3:
+        return nim_best(left_sticks)
 
 def game_controller():
     # via list comprehension
@@ -49,24 +66,31 @@ def game_controller():
     print("3 for Computer vs Computer\n")
 
     choice = int(input("Enter Your choice: "))
-    players = get_player_name_human()
+    players = get_player_names(choice)
     
     # who is playing currently
     current_turn = 0
     
     # run game
     while True:
-        if choice == 1 and len(heap) > 0:
-            print(players[current_turn % 2]+"'s turn ")
-            move = nim_human()
+        if len(heap) > 0:
+            print("Total sticks %s and %s's turn %s" %(len(heap) , players[current_turn % 2], current_turn))
+            
+            if choice == 2 and (current_turn % 2) == 0:
+                move = get_moves(len(heap), 3)
+            else:
+                move = get_moves(len(heap), choice)
+            
+            print("Sticks taken: %s" %move)
+            
             heap = execute_on_heap(heap, move)
             
+            print("Left sticks are: %s" %len(heap))
+
             if is_over(heap):  
-                print(players[current_turn % 2]+" won!!!")     
+                print("Hurry!! {} has WON!!!".format(players[current_turn % 2]))     
                 return  
             # for next player
             current_turn += 1
-
-        print(heap)
 
 print(game_controller())
